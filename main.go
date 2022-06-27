0aec619ad15ef5ce21df42d3465d2859a9cca1ef
lvhoushuai <lvhoushuai_vendor@sensetime.com>
2022-06-27 16:51:13 +0800

    add 任务执行测试

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"code_test/app"
 	"code_test/rest"
 	"fmt"
+	"runtime"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	"github.com/tsxylhs/go-starter/log"
 	"github.com/tsxylhs/go-starter/starter"
 	"github.com/tsxylhs/go-starter/web"
 
@@ -20,6 +24,24 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	// var result = make(chan interface{}, 5)
+	// var task = task.Task{
+	// 	CountPool:   5,
+	// 	Name:        "测试",
+	// 	Description: []string{"简单并发模型"},
+	// 	Do:          dosomething,
+	// 	Results:     result,
+	// }
+	//task.Task3()
+
+	// for {
+	// 	v, ok := <-task.Results
+
+	// 	if !ok {
+	// 		break
+	// 	}
+	// 	fmt.Printf("task 读到数据=%v\n", v)
+	// }
 	// var wg sync.WaitGroup
 	// wg.Add(1)
 	// go func(wg *sync.WaitGroup) {
@@ -46,8 +68,26 @@ func main() {
 	if err := g.Wait(); err != nil {
 		panic(err)
 	} else {
-		fmt.Println("started success")
-		fmt.Println("dbDesc", app.WebApi.DB.Desc())
+		log.Logger.Logger.Info("started success")
+		//log.Logger.Logger.Info("dbDesc {}", app.WebApi.DB.Desc().to)
 	}
 
+}
+func dosomething() interface{} {
+	for i := 1; i < 10; i++ {
+		fmt.Println("gid", goID())
+		fmt.Println("i", i)
+
+	}
+	return "你好"
+
+}
+
+func goID() uint64 {
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	b = bytes.TrimPrefix(b, []byte("goroutine "))
+	b = b[:bytes.IndexByte(b, ' ')]
+	n, _ := strconv.ParseUint(string(b), 10, 64)
+	return n
 }
